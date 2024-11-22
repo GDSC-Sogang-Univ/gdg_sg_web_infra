b6006c8642f70e3553d68bd943665e81c15ac8ce
ljy2855 <ljy2855@naver.com>
2024-11-23 02:22:41 +0900

    feat: add stage cloudfront

--- web_infra/cloudfront.py
+++ web_infra/cloudfront.py
@@ -28,12 +28,45 @@ class CloudFrontStack(Stack):
             removal_policy=RemovalPolicy.DESTROY,  # 스택 삭제 시 버킷 삭제
         )
 
+        stage_bucket = s3.Bucket(
+            self,
+            "gdg-web-static-stage",
+            bucket_name="gdg-web-static-stage",
+            website_index_document="index.html",
+            website_error_document="error.html",
+            public_read_access=True,  # 퍼블릭 접근 허용
+            block_public_access=s3.BlockPublicAccess(  # 퍼블릭 액세스 차단 설정
+                block_public_acls=False,  # ACL 기반 차단 해제
+                block_public_policy=False,  # 퍼블릭 버킷 정책 허용
+                ignore_public_acls=True,  # 퍼블릭 ACL 무시
+                restrict_public_buckets=False,  # 퍼블릭 버킷 제한 비활성화
+            ),
+            removal_policy=RemovalPolicy.DESTROY,  # 스택 삭제 시 버킷 삭제
+        )
+
         # CloudFront 배포 생성
         distribution = cloudfront.Distribution(
             self,
             "GdgWebCloudFront",
             default_behavior=cloudfront.BehaviorOptions(
                 origin=origins.S3Origin(bucket),  # S3 버킷을 CloudFront 원본으로 설정
+                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            ),
+            enable_ipv6=False,  # IPv6 비활성화
+            price_class=cloudfront.PriceClass.PRICE_CLASS_200,
+            geo_restriction=cloudfront.GeoRestriction.allowlist(  # 지리적 제한 설정
+                "KR",  # 한국 지역 허용
+            ),
+            default_root_object="index.html",  # 기본 문서 설정
+        )
+
+        stage_distribution = cloudfront.Distribution(
+            self,
+            "GdgWebStageCloudFront",
+            default_behavior=cloudfront.BehaviorOptions(
+                origin=origins.S3Origin(
+                    stage_bucket
+                ),  # S3 버킷을 CloudFront 원본으로 설정
                 viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
             ),
             enable_ipv6=False,  # IPv6 비활성화

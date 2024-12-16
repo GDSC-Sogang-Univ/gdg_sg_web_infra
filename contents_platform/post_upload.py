f2016a445ee911269f39d796b613a4118682c8e2
ljy2855 <ljy2855@naver.com>
2024-12-17 00:51:32 +0900

    feat: add api gateway endpoint

--- contents_platform/post_upload.py
+++ contents_platform/post_upload.py
@@ -1,11 +1,12 @@
 from aws_cdk import CfnOutput, Duration, RemovalPolicy, Stack
+from aws_cdk import aws_apigateway as apigateway
 from aws_cdk import aws_iam as iam
 from aws_cdk import aws_lambda as _lambda
 from aws_cdk import aws_s3 as s3
 from aws_cdk import aws_secretsmanager as secretsmanager
 from constructs import Construct
 
-NOTION_DATABASE_ID = "15c9bd93ac5d80fda778f4273574f700"
+NOTION_DATABASE_ID = "2c248e8d495b4722b002958aa4b8e70e"
 
 
 class PostUploadStack(Stack):
@@ -61,12 +62,88 @@ class PostUploadStack(Stack):
             )
         )
 
-        # Lambda 함수의 ARN을 출력
+        # API Gateway 생성
+        api = apigateway.RestApi(
+            self,
+            "PostUploadApi",
+            rest_api_name="PostUploadService",
+            description="API Gateway for the PostUpload Lambda function.",
+        )
+
+        # Lambda 통합 추가
+        post_upload_integration = apigateway.LambdaIntegration(
+            post_upload_lambda,
+            proxy=True,  # 요청을 Lambda에 그대로 전달
+        )
+
+        # API 경로 및 메서드 추가
+        api_resource = api.root.add_resource("upload")
+        api_resource.add_method(
+            "POST",
+            post_upload_integration,
+            method_responses=[
+                apigateway.MethodResponse(
+                    status_code="200",
+                    response_parameters={
+                        "method.response.header.Access-Control-Allow-Origin": True,
+                        "method.response.header.Access-Control-Allow-Headers": True,
+                    },
+                ),
+                apigateway.MethodResponse(
+                    status_code="403",
+                    response_parameters={
+                        "method.response.header.Access-Control-Allow-Origin": True,
+                    },
+                ),
+            ],
+            request_parameters={
+                "method.request.header.Authorization": True,
+            },
+        )
+
+        # API Gateway에 CORS 추가
+        api.root.add_method(
+            "OPTIONS",
+            apigateway.MockIntegration(
+                integration_responses=[
+                    apigateway.IntegrationResponse(
+                        status_code="200",
+                        response_parameters={
+                            "method.response.header.Access-Control-Allow-Headers": "'Authorization'",
+                            "method.response.header.Access-Control-Allow-Methods": "'OPTIONS,POST'",
+                            "method.response.header.Access-Control-Allow-Origin": "'*'",
+                        },
+                    )
+                ],
+                passthrough_behavior=apigateway.PassthroughBehavior.NEVER,
+                request_templates={"application/json": '{"statusCode": 200}'},
+            ),
+            method_responses=[
+                apigateway.MethodResponse(
+                    status_code="200",
+                    response_parameters={
+                        "method.response.header.Access-Control-Allow-Headers": True,
+                        "method.response.header.Access-Control-Allow-Methods": True,
+                        "method.response.header.Access-Control-Allow-Origin": True,
+                    },
+                )
+            ],
+        )
+
+        # Lambda 함수의 ARN 출력
         CfnOutput(
             self,
             "LambdaFunctionArn",
             value=post_upload_lambda.function_arn,
             description="ARN of the Lambda function",
+        )
+
+        # API Gateway 엔드포인트 URL 출력
+        CfnOutput(
+            self,
+            "ApiEndpoint",
+            value=api.url,
+            description="URL of the API Gateway endpoint (requires Authorization header)",
         )
 
         # S3 버킷 이름 출력
